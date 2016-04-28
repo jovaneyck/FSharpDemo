e5b66754fe6454df03978d8a5511e11ec33792be
prGmtc <jo.vaneyck@gmail.com>
2016-04-28 15:23:36 +0200

    Moved config code to core package

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -3,13 +3,9 @@ namespace BookingApi.WebHost
 open System
 open System.Web.Http
 
-type HttpRouteDefaults = { Controller : string; Id : obj }
+open BookingApi.Infrastructure
 
 type Global() =
     inherit System.Web.HttpApplication()
     member this.Application_Start (sender : obj) (e : EventArgs) =
-        GlobalConfiguration.Configuration.Routes.MapHttpRoute(
-            "DefaultAPI",
-            "{controller}/{id}",
-            { Controller = "Home"; Id = RouteParameter.Optional }) 
-        |> ignore
+        GlobalConfiguration.Configuration |> Configure
---
+++ BookingApi/Infrastructure.fs
@@ -0,0 +1,14 @@
+﻿module BookingApi.Infrastructure
+
+open System.Web.Http
+
+type HttpRouteDefaults = { Controller : string; Id : obj }
+
+let ConfigureRoutes (configuration : HttpConfiguration) =
+    configuration.Routes.MapHttpRoute(
+            "DefaultAPI",
+            "{controller}/{id}",
+            { Controller = "Home"; Id = RouteParameter.Optional }) 
+        |> ignore
+
+let Configure = ConfigureRoutes
