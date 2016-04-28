b3aea1d5a98561454e6d38e493cf6cf932e50648
prGmtc <jo.vaneyck@gmail.com>
2016-04-28 19:55:33 +0200

    decoupling agent from persistence mechanism. This is some serious over-my-head reactive stuff I need to look into further...

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -1,14 +1,42 @@
 namespace BookingApi.WebHost
 
 open System
+open System.Collections.Concurrent
+open System.Reactive
 open System.Web.Http
-
+open FSharp.Reactive
 open BookingApi.Infrastructure
 open BookingApi.Messages
+open BookingApi.Domain.Reservations
+
+type Agent<'T> = MailboxProcessor<'T>
 
 type Global() =
     inherit System.Web.HttpApplication()
     member this.Application_Start (sender : obj) (e : EventArgs) =
+        let maximumCapacity = 10
+        let reservations = ConcurrentBag<Envelope<Reservation>>()
+
+        let reservationSubject = new Subjects.Subject<Envelope<Reservation>>()
+        reservationSubject.Subscribe reservations.Add |> ignore
+
+        let agent = new Agent<Envelope<MakeReservation>>(fun inbox ->
+            let rec loop () = 
+                async{
+                    let! command = inbox.Receive()
+                    let res = reservations |> ToReservations
+                    let handle = Handle maximumCapacity res
+                    let newReservation = handle command
+                    match newReservation with
+                    | Some(r) -> reservationSubject.OnNext r
+                    | None -> ()
+                    return! loop()
+                }
+            loop())
+        do agent.Start()
+
         Configure 
-            (System.Collections.Concurrent.ConcurrentBag<Envelope<Reservation>>())
+            (reservations |> ToReservations)
+            (Observer.Create agent.Post)
             GlobalConfiguration.Configuration
+            
--- BookingApi/Infrastructure.fs
+++ BookingApi/Infrastructure.fs
@@ -3,7 +3,8 @@
 open System
 open System.Net.Http
 open System.Reactive
-
+open FSharp.Reactive
+open FSharp.Control.Reactive
 open System.Web.Http
 open System.Web.Http.Dispatcher
 open System.Web.Http.Controllers
@@ -11,34 +12,16 @@ open BookingApi.HttpApi
 open BookingApi.Messages
 open BookingApi.Domain.Reservations
 
-type Agent<'T> = MailboxProcessor<'T>
-
-type CompositionRoot(reservations: Collections.Concurrent.ConcurrentBag<Envelope<Reservation>>) =
-    let maximumCapacity = 10
-
-    let agent = new Agent<Envelope<MakeReservation>>(fun inbox ->
-        let rec loop () = 
-            async{
-                let! command = inbox.Receive()
-                let res = reservations |> ToReservations
-                let handle = Handle maximumCapacity res
-                let newReservation = handle command
-                match newReservation with
-                | Some(r) -> reservations.Add r
-                | None -> ()
-                return! loop()
-            }
-        loop())
-    do agent.Start()
-
+type CompositionRoot(reservations: IReservations, reservationRequestObserver) =
     interface IHttpControllerActivator with
         member this.Create(request, controllerDescriptor, controllerType) =
             if controllerType = typeof<HomeController> then
                 new HomeController() :> IHttpController
             elif controllerType = typeof<ReservationController> then
                 let c = new ReservationController()
-                let subscription = c.Subscribe agent.Post
-                request.RegisterForDispose subscription
+                c
+                |> Observable.subscribeObserver reservationRequestObserver
+                |> request.RegisterForDispose
                 c :> IHttpController
             else
                 raise
@@ -46,10 +29,10 @@ type CompositionRoot(reservations: Collections.Concurrent.ConcurrentBag<Envelope
                     sprintf "Unknown controller type requested: %O" controllerType,
                     "controllerType")
 
-let ConfigureCompositionRoot reservations (config : HttpConfiguration) =
+let ConfigureCompositionRoot reservations reservationRequestObserver (config : HttpConfiguration) =
     config.Services.Replace(
         typeof<IHttpControllerActivator>,
-        CompositionRoot(reservations))
+        CompositionRoot(reservations, reservationRequestObserver))
 
 type HttpRouteDefaults = { Controller : string; Id : obj }
 
@@ -64,7 +47,7 @@ let ConfigureFormatting (configuration : HttpConfiguration) =
     configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver
         <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
 
-let Configure reservations c = 
+let Configure reservations reservationRequestObserver c = 
     ConfigureRoutes c
-    ConfigureCompositionRoot reservations c
+    ConfigureCompositionRoot reservations reservationRequestObserver c
     ConfigureFormatting c
